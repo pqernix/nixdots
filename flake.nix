150924616776bb1521d51dfc3dd0b6c0b1ee78a5
pqernix <dashingmcplayzz@gmail.com>
2026-06-25 21:50:07 +0530

    no home

--- flake.nix
+++ flake.nix
@@ -5,28 +5,28 @@
 	inputs = {
 		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-		home-manager.url = "github:nix-community/home-manager/master";
-		home-manager.inputs.nixpkgs.follows = "nixpkgs";
+#		home-manager.url = "github:nix-community/home-manager/master";
+#		home-manager.inputs.nixpkgs.follows = "nixpkgs";
 	};
 	
 
 	outputs = { self, nixpkgs, home-manager, ... }@inputs: {
 		nixosConfigurations = {
-			"elitebook" = nixpkgs.lib.nixosSystem {
+			elitebook = nixpkgs.lib.nixosSystem {
 				system = "x86_64-linux";
 				specialArgs = {
 					inherit inputs;
 				};
 				modules = [
 					./hosts/elitebook/configuration.nix
-					 home-manager.nixosModules.home-manager
-
-						{
-						home-manager.useGlobalPkgs = true;
-						home-manager.useUserPackages = true;
-
-						home-manager.users.pqer = import ./home/pqer.nix;
-						}
+#					 home-manager.nixosModules.home-manager
+#
+#						{
+#						home-manager.useGlobalPkgs = true;
+#						home-manager.useUserPackages = true;
+#						home-manager.extraSpecialArgs = { inherit inputs; };
+#						home-manager.users.pqer = import ./home/pqer.nix;
+#						}
 				];
 			};
 		};
--- nixos-modules/systemPackages.nix
+++ nixos-modules/systemPackages.nix
@@ -4,14 +4,14 @@
 	programs.niri.enable = true;
 
 	environment.systemPackages = with pkgs; [
-		git
 		curl
 		wget
 		btop
+		vim
+		git
+		vesktop
+		zapzap
 		alacritty
 		ungoogled-chromium
-		zapzap
-		vesktop
-		vim
 	];
 }
